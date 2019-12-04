529ef57b99f10b24b35d532f24a658ca01656a7a
Misha Ward <wardm5@uw.edu>
2019-12-04 12:54:54 -0800

    added CSS and JavaScript to show countdown

--- popup.js
+++ popup.js
@@ -1,8 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-    chrome.tabs.getSelected(null, function(tab) {
-        alert("hello");
-    });
-  }, false);
-}, false);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   var startTimerButton = document.getElementById('startTimer');
+//   var endTimerButton = document.getElementById('endTimer');
+//   checkPageButton.addEventListener('click', function() {
+//     chrome.tabs.getSelected(null, function(tab) {
+//         alert("hello");
+//     });
+//   }, false);
+//
+//   // start timer button
+//   startTimerButton.addEventListener('click', function() {
+//     chrome.tabs.getSelected(null, function(tab) {
+//         alert("start");
+//     });
+//   }, false);
+//
+//   // end timer button
+//   endTimerButton.addEventListener('click', function() {
+//     chrome.tabs.getSelected(null, function(tab) {
+//         alert("end");
+//     });
+//   }, false);
+// }, false);
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),  // set this
+    x = setInterval(function() {
+      let now = new Date().getTime(),
+          distance = countDown - now;
+      // document.getElementById('days').innerText = Math.floor(distance / (day)),
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+      //do something later when date is reached
+      //if (distance < 0) {
+      //  clearInterval(x);
+      //  'IT'S MY BIRTHDAY!;
+      //}
+    }, second)
