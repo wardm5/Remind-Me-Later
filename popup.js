b764e2805ab41955963a005a18684840cefd26fc
Misha Ward <wardm5@uw.edu>
2019-12-04 14:05:25 -0800

    updated css, javascript, and html for new clock format

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
-const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
-
+// https://codepen.io/AllThingsSmitty/pen/JJavZN
+//
+// const second = 1000,
+//       minute = second * 60,
+//       hour = minute * 60,
+//       day = hour * 24;
+//
 document.addEventListener('DOMContentLoaded', function() {
   var startTimerButton = document.getElementById('startTimer');
   var endTimerButton = document.getElementById('endTimer');
@@ -21,19 +23,63 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }, false);
 }, false);
+//
+// let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),  // set this
+//     x = setInterval(function() {
+//       let now = new Date().getTime(),
+//           distance = countDown - now;
+//       // document.getElementById('days').innerText = Math.floor(distance / (day)),
+//       document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+//       document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+//       document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+//
+//       //do something later when date is reached
+//       //if (distance < 0) {
+//       //  clearInterval(x);
+//       //  'IT'S MY BIRTHDAY!;
+//       //}
+//     }, second)
 
-let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),  // set this
-    x = setInterval(function() {
-      let now = new Date().getTime(),
-          distance = countDown - now;
-      // document.getElementById('days').innerText = Math.floor(distance / (day)),
-      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+//  https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
 
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
-    }, second)
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  // var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    // 'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  // var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    // daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 1 * 2 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
