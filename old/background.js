f267f163ff6c78c7ff2ebb2b1670624e2272a4b4
Misha Ward <wardm5@uw.edu>
2019-12-08 21:57:56 -0800

    made small updates/reduced misc comments/unneeded code

--- old/background.js
+++ old/background.js
@@ -1,40 +1,40 @@
-// var hours;
-// var minutes;
-// var seconds;
-// var endTime;
-// var timer;
-// var created = false;
-// var paused;
-//
-// function start() {
-//     if (created) {
-//         timer = setTimeout(function() {
-//             if (!paused) {
-//                 PopupCenter('reminder.html', 'mywin', 315, 250);
-//             }
-//         }, ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds) * 1000));
-//     }
-// }
-//
-// function pause() {
-//     clearTimeout(timer);
-//     paused = true;
-// }
-//
-// function resume() {
-//     paused = false;
-//     if (created) {
-//         timer = setTimeout(function() {
-//             if (!paused) {
-//                 PopupCenter('reminder.html', 'mywin', 315, 250);
-//             }
-//         }, ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds) * 1000));
-//     }
-// }
-//
-// function PopupCenter(pageURL, title,w,h) {
-//   var left = (screen.width/2)-(w/2);
-//   var top = (screen.height/2)-(h/2);
-//   var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-//   return targetWin;
-// }
+var hours;
+var minutes;
+var seconds;
+var endTime;
+var timer;
+var created = false;
+var paused;
+
+function start() {
+    if (created) {
+        timer = setTimeout(function() {
+            if (!paused) {
+                PopupCenter('reminder.html', 'mywin', 315, 250);
+            }
+        }, ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds) * 1000));
+    }
+}
+
+function pause() {
+    clearTimeout(timer);
+    paused = true;
+}
+
+function resume() {
+    paused = false;
+    if (created) {
+        timer = setTimeout(function() {
+            if (!paused) {
+                PopupCenter('reminder.html', 'mywin', 315, 250);
+            }
+        }, ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds) * 1000));
+    }
+}
+
+function PopupCenter(pageURL, title,w,h) {
+  var left = (screen.width/2)-(w/2);
+  var top = (screen.height/2)-(h/2);
+  var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+  return targetWin;
+}
--- popup.js
+++ popup.js
@@ -2,9 +2,9 @@ var inputHours = 0;
 var inputMinutes = 0;
 var inputSeconds = 0;
 
-var defaultHours = 2;
+var defaultHours = 0;
 var defaultMinutes = 1;
-var defaultSeconds = 01;
+var defaultSeconds = 1;
 
 initializeClock();
 
@@ -14,13 +14,6 @@ function initializeClock() {
             backgroundPage.start();
         } else {
             setClock(0, 0, 0);
-            // var clock = document.getElementById('clockdiv');
-            // var hoursSpan = clock.querySelector('.hours');
-            // var minutesSpan = clock.querySelector('.minutes');
-            // var secondsSpan = clock.querySelector('.seconds');
-            // hoursSpan.innerHTML = ('0' + 0).slice(-2);
-            // minutesSpan.innerHTML = ('0' + 0).slice(-2);
-            // secondsSpan.innerHTML = ('0' + 0).slice(-2);
         }
     });
 }
@@ -53,24 +46,17 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.msg === "updateTime") {
             setClock(request.data.hours, request.data.minutes, request.data.seconds);
-            // var clock = document.getElementById('clockdiv');
-            // var hoursSpan = clock.querySelector('.hours');
-            // var minutesSpan = clock.querySelector('.minutes');
-            // var secondsSpan = clock.querySelector('.seconds');
-            // hoursSpan.innerHTML = ('0' + request.data.hours).slice(-2);
-            // minutesSpan.innerHTML = ('0' + request.data.minutes).slice(-2);
-            // secondsSpan.innerHTML = ('0' + request.data.seconds).slice(-2);
         }
     }
 );
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.msg === "initialize") {
-
-        }
-    }
-);
+// chrome.runtime.onMessage.addListener(
+//     function(request, sender, sendResponse) {
+//         if (request.msg === "initialize") {
+//
+//         }
+//     }
+// );
 
 function setClock(hours, minutes, seconds) {
     var clock = document.getElementById('clockdiv');
